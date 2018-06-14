a9effffb60ef070f522dcb17d68cfb73a11a8bd5
atkinsta <atkinstaa@gmail.com>
2018-06-14 17:04:34 -0400

    added functioning trigger switching buttons

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+var hoverOn = true;
 var baseURL = "https://api.giphy.com/v1/gifs/search?api_key=BDU3KiPFpEdQOmsEDOWmrTfIrkdzo4Ji&limit=25&offset=0&rating=PG&lang=en&q="
 
-$("#submit").on("click", function() {
+$("#submit").on("click", function () {
     $("#contentholder").empty();
     var searchTerm = $("#searchterm").val();
     var queryURL = baseURL + searchTerm;
@@ -10,7 +11,7 @@ $("#submit").on("click", function() {
     }).then(function (response) {
         console.log(response);
         var data = response.data;
-        for (var i = 0; i < 25; i++){
+        for (var i = 0; i < 25; i++) {
             var newImage = $("<img>");
             newImage.attr("class", "content");
             newImage.attr("src", data[i].images.fixed_height_still.url);
@@ -22,9 +23,36 @@ $("#submit").on("click", function() {
     });
 });
 
-$(document).on("mouseover", ".content", function() {
-    $(this).attr("src", $(this).attr("data-animated"));
-    $(document).on("mouseout", ".content", function() {
-        $(this).attr("src", $(this).attr("data-still"));
-    });
+$(".controls").on("click", function () {
+    console.log($(this).val());
+    if ($(this).val() === "click") {
+        hoverOn = false;
+        console.log(hoverOn);
+    }
+    else if ($(this).val() === "hover") {
+        hoverOn = true;
+        console.log(hoverOn);
+    }
+});
+
+
+$(document).on("mouseover", ".content", function () {
+    if (hoverOn) { //Alows the user to choose which trigger they would like to use to play the gifs. Ignores mouseover if hoverOn is true. 
+        $(this).attr("src", $(this).attr("data-animated"));
+        $(document).on("mouseout", ".content", function () {
+            $(this).attr("src", $(this).attr("data-still"));
+        });
+    }
+});
+
+$(document).on("click", ".content", function () {
+    if (!hoverOn) { //same function as above, but ignores clicks if hoverOn is false. 
+        if ($(this).attr("data-playing") == "no") {
+            $(this).attr("data-playing", "yes");
+            $(this).attr("src", $(this).attr("data-animated"));
+        }
+        else {
+            $(this).attr("src", $(this).attr("data-still"));
+        }
+    }
 });
